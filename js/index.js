17addcf2cf521b05885998ec397b93d88b01f7ae
Taylor Ward <taylortheward@gmail.com>
2018-05-24 23:57:39 -0700

    Made info-panel work on mobile / responsive

--- js/index.js
+++ js/index.js
@@ -1,110 +1,135 @@
 (function() {
 
-    const infoPanelOne = document.querySelectorAll('.info-panel')[0];
-    const infoPanelTwo = document.querySelectorAll('.info-panel')[1];
-    const infoPanelThree = document.querySelectorAll('.info-panel')[2];
+    const infoPanels = document.querySelectorAll('.info-panel');
 
-    const projectOne = document.querySelectorAll('.project')[0];
-    const projectTwo = document.querySelectorAll('.project')[1];
-    const projectThree = document.querySelectorAll('.project')[2];
+    const projects = document.querySelectorAll('.project');
 
-    const projectBGOne = document.querySelectorAll('.project__background')[0];
-    const projectBGTwo = document.querySelectorAll('.project__background')[1];
-    const projectBGThree = document.querySelectorAll('.project__background')[2];
+    const projectBGs = document.querySelectorAll('.bg__text');
     
-    const projectOneY = projectOne.offsetTop;
-    const projectTwoY = projectTwo.offsetTop;
-    const projectThreeY = projectThree.offsetTop;
-    
+    const container = document.querySelector('.container');
 
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.pageYOffset;
-        // for each scroll event
-        // Check first if the scroll position (top of screen) 
-        // has reached the top of project one 
-        if (scrollPosition > projectOneY) {
-            // if it has we then want to check if 
-            // the project-one-panel is already showing
-            if (infoPanelOne.className.indexOf(` show`) == -1) {
-                // if it isn't then add class that will show it
-                infoPanelOne.classList.add('show');
-                infoPanelOne.classList.remove('hide');
-                
-                projectBGOne.classList.add('show');
-                projectBGOne.classList.remove('hide');
-                // If panel-one is already showing then 
-                // check if we are at project two yet
-            } else if (scrollPosition > projectTwoY) {
-                // if we are then check if panel-two is showing
-                if (infoPanelTwo.className.indexOf(` show`) == -1) {
-                    // if it is not then make it show
-                    infoPanelTwo.classList.add('show');
-                    infoPanelTwo.classList.remove('hide');
+    let waitingForUpdate;
 
-                    projectBGTwo.classList.add('show');
-                    projectBGTwo.classList.remove('hide');
+    let currentProject = -1;
 
-                    projectBGOne.classList.add('hide');
-                    projectBGOne.classList.remove('show');
+    const moreInfo = document.querySelectorAll('.info-panel__expand');
 
-                    // If panel-one and panel-two are both already
-                    // showing then we need to check to see if we need 
-                    // to show panel-three
-                } else if (scrollPosition > projectThreeY) {
-                    // if scroll position is past project three trigger point
-                    // then check if panel-three is already showing
-                    if (infoPanelThree.className.indexOf(` show`) == -1) {
-                        // if not then make it show
-                        infoPanelThree.classList.add('show');
-                        infoPanelThree.classList.remove('hide');
+    addMultipleEventListeners(moreInfo, 'click', (event, i) => {
 
-                        projectBGThree.classList.add('show');
-                        projectBGThree.classList.remove('hide');
+        toggleInfoExpansion(event, i);
 
-                        projectBGTwo.classList.add('hide');
-                        projectBGTwo.classList.remove('show');
-
-                    }
-                } else if (infoPanelThree.className.indexOf(` show`) !== -1) {
-
-                    infoPanelThree.classList.add('hide');
-                    infoPanelThree.classList.remove('show');
-
-                    projectBGThree.classList.add('hide');
-                    projectBGThree.classList.remove('show');
-
-                    projectBGTwo.classList.add('show');
-                    projectBGTwo.classList.remove('hide');
-
-                }
-            } else if (infoPanelTwo.className.indexOf(` show`) !== -1) {
-                // if we are above projectTwo but panel-two is showing
-                // hide panel two
-                infoPanelTwo.classList.add('hide');
-                infoPanelTwo.classList.remove('show');
-
-                projectBGTwo.classList.add('hide');
-                projectBGTwo.classList.remove('show');
-
-                projectBGOne.classList.add('show');
-                projectBGOne.classList.remove('hide');
-            }
-             
-
-        } else if (infoPanelOne.className.indexOf(` show`) !== -1) {
-            // if we are above the project-one section but 
-            // project-one-panel is showing i.e we are moving
-            // up - remove the show class and hide panel
-            
-            infoPanelOne.classList.add('hide');
-            infoPanelOne.classList.remove('show');
-
-            projectBGOne.classList.add('hide');
-            projectBGOne.classList.remove('show');
-
-        }
     })
 
-    
+    // Use window.addEventListener & window.pageYOffset if not using parallax
+    container.addEventListener('scroll', () => {
+        
+        if (!waitingForUpdate) {
+            
+            window.requestAnimationFrame(scrollUpdate);
 
-})()
+            waitingForUpdate = true;
+
+        }
+        
+    })
+
+    function scrollUpdate() {
+
+        waitingForUpdate = false;               // For debouncing the scroll event handler
+
+        const scrollPosition = container.scrollTop;
+
+        const currentTop = currentProject !== -1
+            ? projects[currentProject].offsetTop 
+            : 0;
+
+        const currentHeight = currentProject !== -1    
+            ? projects[currentProject].offsetHeight
+            : projects[0].offsetTop;
+
+        if (scrollPosition > currentTop + currentHeight) {
+            
+            if (currentProject !== -1) {
+
+                hide(projectBGs[currentProject]);
+
+                infoPanels[currentProject].classList.remove('expanded');
+
+            }
+
+            currentProject++;
+            
+            show(infoPanels[currentProject], projectBGs[currentProject]);
+
+        } else if (scrollPosition < currentTop) {
+            
+            hide(infoPanels[currentProject], projectBGs[currentProject]);
+
+            infoPanels[currentProject].classList.remove('expanded');
+
+            currentProject--;
+
+            if (currentProject !== -1) show(projectBGs[currentProject]);
+            
+        }
+
+    }
+
+    function addMultipleEventListeners(arr, event, callback) {
+
+        arr.forEach((el, i) => {
+
+            el.addEventListener(event, (e) => {
+                
+                callback(e, i);
+
+            });
+
+        })
+
+    }
+
+    function show() {
+        
+        for (let i=0; i < arguments.length; i++) {
+
+            arguments[i].classList.add('show');
+
+            arguments[i].classList.remove('hide');
+        }
+
+    }
+
+    function hide() {
+
+        for (let i=0; i < arguments.length; i++) {
+
+            arguments[i].classList.add('hide');
+
+            arguments[i].classList.remove('show');
+            
+        }
+
+    }
+
+    function toggleInfoExpansion(event, i) {
+
+        // first we need to determine whether we need to open or close the 
+        // info panel. We can do this by checking if the expanded class is 
+        // present or not. If it is then we need to remove it if it's not
+        // we need to add it.
+        if (infoPanels[i].className.indexOf(' expanded') == -1) {
+            // then panel is not expanded and we should add the class
+            infoPanels[i].classList.add('expanded');
+            moreInfo[i].innerHTML = "less info";
+            
+        } else {
+
+            infoPanels[i].classList.remove('expanded');
+            moreInfo[i].innerHTML = "more info";
+
+        }
+
+    }
+
+})();
